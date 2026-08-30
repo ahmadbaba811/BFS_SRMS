f7819030b5d5923c98aa3c6b0564e46f3d7f709a
Ahmad Idris <ahmadbaba811@hotmail.com>
2026-08-30 15:54:18 +0100

    added sqllite

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
+
+
+DB_PATH = 'db/srms_db.db'
+
 
 app = Flask(__name__)
 
@@ -26,9 +31,22 @@ def teachers():
 def subjects():
     return render_template('subjects.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST', 'PATCH', 'DELETE'])
 def login():
-    return render_template('login.html')
+    query_result = None
+    if request.method == 'POST':
+        # run a sqlite3 query when the button is clicked
+        conn = sqlite3.connect(DB_PATH)
+        cursor = conn.cursor()
+        cursor.execute('SELECT COUNT(*) FROM tbl_students')
+        ## [{id:1, regNo: 'U21AA1003', ....}, {}, {}]
+        query_result = cursor.fetchone();
+        for row in query_result:
+            print(row);
+        
+        conn.close()
+
+    return render_template('login.html', query_result=query_result)
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
